c9e3881e6ba87a34ff2bcaba92867609b981fd26
Vitaly Kruglikov <vkruglikov@vitalys-mbp.corp.numenta.com>
2016-07-19 14:38:20 -0700

    DEVOPS-116 Use quay.io instead of docker hub registry

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker login -e noemail -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
-for tag in quay.io/pypa/manylinux1_i686 quay.io/pypa/manylinux1_x86_64; do
+for tag in quay.io/numenta/manylinux1_x86_64_centos6; do
     docker tag ${tag}:${COMMIT} ${tag}:latest
     docker push ${tag}:latest
 done
